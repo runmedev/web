988970046c0a8a012baf09051b55ea98da4a7ff0
Jeremy lewi <jeremy@lewi.us>
2026-04-26 11:17:10 -0700

    Make chat side panel fill available width

--- app/src/components/ChatKit/ChatKitPanel.tsx
+++ app/src/components/ChatKit/ChatKitPanel.tsx
@@ -1312,7 +1312,7 @@ function ChatKitPanel() {
   )
 
   return (
-    <div className="flex h-full min-h-0 flex-col">
+    <div className="flex h-full min-h-0 w-full min-w-0 flex-col">
       <div className="min-h-0 flex-1">
         <ChatKitPanelInner
           key={harnessSessionKey}
